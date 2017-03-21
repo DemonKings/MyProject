32b8cce7796fa7e75d726ebed2f645f4888aede1
weitong <wei1213188874@qq.com>
2017-03-22 00:07:56 +0800

    任务完成

--- src/com/example/yonghuzhuce/Activity2.java
+++ src/com/example/yonghuzhuce/Activity2.java
@@ -1,11 +1,36 @@
 package com.example.yonghuzhuce;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 public class Activity2 extends Activity {
+	private EditText ed_yhm;
+	private EditText ed_mm;	
 	
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);      
+    }
+	public void login(View view){
+		ed_yhm =(EditText) findViewById(R.id.editText1);
+		ed_mm =(EditText) findViewById(R.id.editText2);
+		
+		String yhm=ed_yhm.getText().toString().trim();
+		String mm=ed_mm.getText().toString().trim();
+		
+		if((yhm.contentEquals("lidengming")&&mm.contentEquals("139")) ||
+				(yhm.contentEquals("weitong")&&mm.contentEquals("142")) ||
+				(yhm.contentEquals("zhaoxuechen")&&mm.contentEquals("140"))
+		){
+	    	Intent intent=new Intent(this,Activity3.class);
+	    	intent.putExtra("mm",ed_mm.getText().toString().trim());
+	    	startActivity(intent);
+		}else{
+			(Toast.makeText(Activity2.this,"’À∫≈ªÚ√‹¬Î¥ÌŒÛ",Toast.LENGTH_LONG)).show();
+		}
+
     }
 }
--- src/com/example/yonghuzhuce/Activity3.java
+++ src/com/example/yonghuzhuce/Activity3.java
@@ -1,14 +1,25 @@
 package com.example.yonghuzhuce;
-import com.example.yonghuzhuce.R;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 
 public class Activity3 extends Activity {
+	
+	private ImageView im;
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity3);      
+        setContentView(R.layout.activity3);     
+        
+        Intent intent=getIntent();
+        String mm=intent.getStringExtra("mm");
+        im =(ImageView) findViewById(R.id.imageView1);
+        if(mm.contentEquals("142")){
+        	im.setImageResource(R.drawable.wei);
+        }else if(mm.contentEquals("140")){
+        	im.setImageResource(R.drawable.lei);
+        }
     }
 
 }
