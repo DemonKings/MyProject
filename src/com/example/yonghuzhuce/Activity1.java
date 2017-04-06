b5078d761c8911ab4f97fd27878262806a70fba7
weitong <wei1213188874@qq.com>
2017-04-06 21:45:25 +0800

    第四次作业

--- src/com/example/yonghuzhuce/Activity1.java
+++ src/com/example/yonghuzhuce/Activity1.java
@@ -1,24 +1,28 @@
 package com.example.yonghuzhuce;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.shujuku.MyHelper;
+
 public class Activity1 extends Activity {
 	//定义
+	private MyHelper helper;
 	private EditText editText1;
 	private TextView textView4,textView5,textView7;
 	private EditText extra_yhm;
 	private TextView extra_mm,extra_xb;
-	
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity1);
+        
+        helper = new MyHelper(this);
         
         //获取Intent对象
         Intent intent=getIntent();
@@ -43,21 +47,29 @@ public class Activity1 extends Activity {
     }
     // 单击时间名字“click”应与该控件onClick值一致
     public void click(View view){    //创建单击事件  回传数据
+    	
     	extra_yhm =(EditText) findViewById(R.id.editText1);
-    	extra_mm=(TextView) findViewById(R.id.textView4);
-    	extra_xb=(TextView) findViewById(R.id.textView5);
+    	extra_mm =(TextView) findViewById(R.id.textView4);
+    	extra_xb =(TextView) findViewById(R.id.textView5);
     	Intent intent =new Intent(); //创建Intent对象
     	intent.putExtra("extra_data",extra_yhm.getText().toString().trim());
     	setResult(1,intent);
-    	SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
-    	Editor editor=sp.edit();
-    	editor.putString("yhm", extra_yhm.getText().toString().trim());
-    	editor.putString("mm", extra_mm.getText().toString().trim());
-    	editor.putString("xb", extra_xb.getText().toString().trim());
-    	editor.putString("ischeck", null);
-    	editor.commit();
+    	String yhm=extra_yhm.getText().toString().trim();
+    	String mm=extra_mm.getText().toString().trim();
+    	String xb=extra_xb.getText().toString().trim();
+    	add(yhm, mm, xb);
     	finish();  //销毁activity
     	
     }
+    public long add(String yhm,String mm,String xb){
+		SQLiteDatabase db=helper.getWritableDatabase();
+		ContentValues values=new ContentValues();
+		values.put("yhm", yhm);
+		values.put("mm", mm);
+		values.put("xb", xb);
+		long id=db.insert("info", null, values);
+		db.close();
+		return id;
+	}
 
 }
--- src/com/example/yonghuzhuce/Activity2.java
+++ src/com/example/yonghuzhuce/Activity2.java
@@ -2,36 +2,35 @@ package com.example.yonghuzhuce;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.example.shujuku.MyHelper;
 public class Activity2 extends Activity {
 	private EditText ed_yhm;
 	private EditText ed_mm;	
-	private CheckBox check;
+	private MyHelper helper;
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);  
         ed_yhm =(EditText) findViewById(R.id.editText1);
-        SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
-        String ch=sp.getString("ischeck", null);
-        ed_yhm.setText(ch);
+        ed_mm =(EditText) findViewById(R.id.editText2);
+        helper = new MyHelper(this);
     }
+
 	public void login(View view){
-        SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
-        String getyhm=sp.getString("yhm", null);
-        String getmm=sp.getString("mm", null);
 		ed_yhm =(EditText) findViewById(R.id.editText1);
-		ed_mm =(EditText) findViewById(R.id.editText2);
-		
+        ed_mm =(EditText) findViewById(R.id.editText2);
+        
 		String yhm=ed_yhm.getText().toString().trim();
 		String mm=ed_mm.getText().toString().trim();
+		boolean result=find(yhm,mm);
+
 		// 判断所输入内容是否一致。 注意：对象不同，不能用"=="
-		if(yhm.contentEquals(getyhm)&&mm.contentEquals(getmm)){
+		if(result==true){
 	    	Intent intent=new Intent(this,Activity3.class);
 	    	intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
 	    	startActivity(intent);
@@ -39,15 +38,21 @@ public class Activity2 extends Activity {
 		}else{
 			(Toast.makeText(Activity2.this,"登陆失败",Toast.LENGTH_LONG)).show();//创建弹出窗体
 		}
-		check =(CheckBox) findViewById(R.id.checkBox1);
-		if(check.isChecked()){
-	        Editor editor=sp.edit();
-	        editor.putString("ischeck", yhm);
-	        editor.commit();
-		}else{
-			Editor editor=sp.edit();
-			editor.putString("ischeck", null);
-	        editor.commit();
-		}
+
     }
+	
+	public boolean find(String yhm, String mm){
+		SQLiteDatabase db=helper.getReadableDatabase();
+		Cursor cursor = db.rawQuery("SELECT * FROM "+ "info", null);
+		while (cursor.moveToNext()) {
+			String yhmInDB = cursor.getString(cursor.getColumnIndex("yhm"));
+        	String mmInDB = cursor.getString(cursor.getColumnIndex("mm"));
+        	if(yhmInDB.equals(yhm) && mmInDB.equals(mm)){
+        		cursor.close();
+        		return true;
+        	}
+	    }
+		cursor.close();
+		return false;
+	}
 }
